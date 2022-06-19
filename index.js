833489cb42393b0d6297a8b631cd6e9a29aca1a1
kajal parchande <kajal.parchande@greentiger.in>
2022-06-19 10:36:10 +0530

    Express basic api creation

---
+++ index.js
@@ -0,0 +1,22 @@
+// const express = require('express') old version
+import express from 'express';
+const app = express()
+const port = 3400
+
+app.get('/student/all', (req, res) => {
+  res.send('Hello World!')
+});
+app.post('/student/create',(req,res)=>{
+    res.send("create student");
+})
+app.post('/student/update',(req,res)=>{
+    res.send("update student");
+})
+app.post('/student/delete',(req,res)=>{
+    res.send("delete student");
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
